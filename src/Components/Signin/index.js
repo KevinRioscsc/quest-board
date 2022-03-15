77b1811f68d895a4717a55b01b23c7250b8175bd
Kevin Rios <rioskevin89@gmail.com>
2022-03-14 23:20:50 -0700

    quest

--- src/Components/Signin/index.js
+++ src/Components/Signin/index.js
@@ -32,7 +32,7 @@ const SigninPage = () => {
           console.log(result.user.displayName)
           setLogin(true)
       }).catch((error) => {
-          console.log(error)
+          console.log(error, 'poopy')
       });
   }
   return (
